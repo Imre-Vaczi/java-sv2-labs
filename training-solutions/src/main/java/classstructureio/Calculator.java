544f9ada874f4bb0faf276c9806329f435083330
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-16 18:31:26 +0200

    Calculator class uploaded

---
+++ training-solutions/src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,15 @@
+package classstructureio;
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please provide an integer: ");
+        int firstInt = scanner.nextInt();
+        System.out.println("Please provide an other integer: ");
+        int secInt = scanner.nextInt();
+        int result = firstInt + secInt;
+        System.out.println(Integer.toString(firstInt) + " + " + Integer.toString(secInt));
+        System.out.println("(" + result + ")");
+
+    }
+}
