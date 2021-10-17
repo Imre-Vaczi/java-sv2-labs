b445e2d3ba694d0c8a609a94cc558c1740769733
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-17 16:17:16 +0200

    Store class updated

---
+++ training-solutions/src/main/java/classstructureconstructors/Store.java
@@ -0,0 +1,17 @@
+package classstructureconstructors;
+
+public class Store {
+    private String product;
+    private int stock;
+
+    public Store(String product){
+        this.product = product;
+        this.stock = 0;
+    }
+
+    public String getProduct(){return product;}
+    public int getStock(){return stock;}
+
+    public void store(int stock){this.stock = stock;}
+    public void dispatch(int dispatchQty){this.stock = this.stock - dispatchQty;}
+}
