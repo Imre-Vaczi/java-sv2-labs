91f44b5d0c2145cd7a223a0845b19cbb8636300f
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-19 23:04:52 +0200

    UpperMain class updated

---
+++ training-solutions/src/main/java/javaapi/UpperMain.java
@@ -0,0 +1,8 @@
+package javaapi;
+
+public class UpperMain {
+    public static void main(String[] args) {
+        Upper upper = new Upper("Hello World!");
+        System.out.println(upper.upperate());
+    }
+}
