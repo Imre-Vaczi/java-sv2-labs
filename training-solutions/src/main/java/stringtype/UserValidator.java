aec2444ad2ffb0879eac6bf55987ffb151647e6e
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-20 20:31:49 +0200

    UserValidator class updated

---
+++ training-solutions/src/main/java/stringtype/UserValidator.java
@@ -0,0 +1,18 @@
+package stringtype;
+
+public class UserValidator {
+
+    public boolean isValidUsername(String username){
+        return username.length() > 0 ? true : false;
+    }
+
+    public boolean isValidPassword(String password1, String password2){
+        return password1.equals(password2) & password1.length() > 7;
+    }
+
+    public boolean isValidEmail(String email){
+        boolean isAtOk = email.indexOf("@") > 0;
+        boolean isDotOK = email.indexOf(".") > email.indexOf("@") +1 & email.indexOf(".") > 0 & email.length()-1 > email.indexOf(".");
+        return isAtOk & isDotOK;
+    }
+}
