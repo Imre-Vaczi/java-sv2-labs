6e55f720fdf0b573feeb4c21c5b388e99f1f2490
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-20 20:40:40 +0200

    UserValidator class updated

--- training-solutions/src/main/java/stringtype/UserValidator.java
+++ training-solutions/src/main/java/stringtype/UserValidator.java
@@ -3,7 +3,7 @@ package stringtype;
 public class UserValidator {
 
     public boolean isValidUsername(String username){
-        return username.length() > 0 ? true : false;
+        return username.length() > 0;
     }
 
     public boolean isValidPassword(String password1, String password2){
