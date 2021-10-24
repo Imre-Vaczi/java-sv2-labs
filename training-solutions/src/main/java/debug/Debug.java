f46b9452178b711d7b4aba3f8ad3ef511deee55f
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-24 14:05:24 +0200

    Debug class updated

--- training-solutions/src/main/java/debug/Debug.java
+++ training-solutions/src/main/java/debug/Debug.java
@@ -7,17 +7,18 @@ public class Debug {
         List<String> fruits = Arrays.asList("alma", "körte", "barack", "szilva");
 
         String peach = fruits.get(2);
-        fruits.set(3, peach);
+        fruits.set(1, peach);
         for (int i = 0; i < fruits.size(); i++) {
             String fruit = fruits.get(i);
             fruits.contains(fruit.substring(0, 2));
         }
-        System.out.println(fruits.get(1));
+        System.out.println(fruits.get(1).substring(0, 2));
 
         int[] numbers = {3, 7, -2, 1, -4, 5};
 
-        numbers[2] = 0;
+        //numbers[2] = 0;
         for (int i = 0; i < numbers.length; i++) {
+            numbers[2] = 0;
             numbers[2]++;
             numbers[2]++;
         }
