2ce139ed09c15240966eb5dddfeea433a055e73f
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-24 15:18:29 +0200

    MathMethods class updated

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,20 @@
+package math.math;
+
+public class MathMethods {
+    public static void main(String[] args) {
+        System.out.println(Math.max(10,20));
+        System.out.println(Math.min(10,20));
+        System.out.println(Math.PI);
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.round(9.44445342));
+        System.out.println(Math.abs(-4));
+        System.out.println(Math.negateExact(1));
+        System.out.println(Math.addExact(20,10));
+        System.out.println(Math.subtractExact(10,20));
+        System.out.println(Math.multiplyExact(10,20));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.incrementExact(10));
+        System.out.println(Math.decrementExact(10));
+        System.out.println(Math.random());
+    }
+}
