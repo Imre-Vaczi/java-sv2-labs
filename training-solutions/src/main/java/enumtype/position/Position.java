b342be56bc8828253743121e581a39a72a0d7735
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-30 17:25:57 +0200

    Position classes updated

---
+++ training-solutions/src/main/java/enumtype/position/Position.java
@@ -0,0 +1,21 @@
+package enumtype.position;
+
+public enum Position {
+    ARCHITECT(1000, "free pencil"), DESIGNER(1000, "free brush");
+
+    private final int salary;
+    private final String benefit;
+
+    Position(int salary, String benefit) {
+        this.salary = salary;
+        this.benefit = benefit;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getBenefit() {
+        return benefit;
+    }
+}
