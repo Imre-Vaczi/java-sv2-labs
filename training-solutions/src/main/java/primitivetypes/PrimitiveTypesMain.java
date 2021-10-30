03fd862e88915c7a1fca9faef2336cb7820ca190
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-30 11:48:38 +0200

    PrimitiveTypesMain class updated

--- training-solutions/src/main/java/primitivetypes/PrimitiveTypesMain.java
+++ training-solutions/src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -12,4 +12,13 @@ public class PrimitiveTypesMain {
     int h = Integer.valueOf(c);
     int i = Integer.parseInt("2");
     Integer j = Integer.parseInt("2");
+
+    boolean k = true;
+    boolean l = false;
+    boolean m = Boolean.valueOf(true);
+    boolean n = Boolean.valueOf(false);
+    boolean o = Boolean.parseBoolean("TrUe");
+    Boolean p = Boolean.parseBoolean("TrUe");
+    boolean q = Boolean.parseBoolean("TreU");
+    Boolean r = Boolean.parseBoolean("TreU");
 }
