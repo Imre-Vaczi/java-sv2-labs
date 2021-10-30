78a3a32a0fd8793ebc10b880d609cc01f2cd0295
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-30 11:40:45 +0200

    PrimitiveTypesMain class updated

---
+++ training-solutions/src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -0,0 +1,15 @@
+package primitivetypes;
+
+public class PrimitiveTypesMain {
+
+    int a = 2;
+    Integer b = 2;
+    Integer c = a;
+    Integer d = b;
+    Integer e = Integer.valueOf(2);
+    int f = Integer.valueOf(2);
+    int g = Integer.valueOf(a);
+    int h = Integer.valueOf(c);
+    int i = Integer.parseInt("2");
+    Integer j = Integer.parseInt("2");
+}
