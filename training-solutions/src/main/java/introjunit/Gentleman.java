f0d7fb97e1d867532a2dd451c3e5269589f69a9f
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-02 17:36:14 +0100

    Gentleman class updated

--- training-solutions/src/main/java/introjunit/Gentleman.java
+++ training-solutions/src/main/java/introjunit/Gentleman.java
@@ -3,6 +3,11 @@ package introjunit;
 public class Gentleman {
     private String message = "Hello ";
     public String sayHello(String name){
-        return this.message + name;
+        if (name == null){
+            return this.message + "Anonymus";
+        }else{
+            return this.message + name;
+        }
+
     }
 }
