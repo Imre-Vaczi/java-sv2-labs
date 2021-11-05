093a89690a5b2547622cf50c1d7fc63c7c59c8ec
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-05 23:54:57 +0100

    Exit class updated

---
+++ training-solutions/controliteration/src/main/java/exit/Exit.java
@@ -0,0 +1,15 @@
+package exit;
+
+import java.util.Scanner;
+
+public class Exit {
+    public static void main(String[] args) {
+        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("x")){
+            System.out.println("Az ügyfélszolgálat menüje jelenleg nem elérhető. \nKilépés: x billentyű megnyomásával");
+            input = scanner.nextLine();
+            }
+        }
+    }
