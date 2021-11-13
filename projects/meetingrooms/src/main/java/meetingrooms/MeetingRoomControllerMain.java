3f5eb92276fbe8f0ad49283634bb3180181ae3b6
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-13 11:48:54 +0100

    cleaning code

---
+++ projects/meetingrooms/src/main/java/meetingrooms/MeetingRoomControllerMain.java
@@ -0,0 +1,8 @@
+package meetingrooms;
+
+public class MeetingRoomControllerMain {
+    public static void main(String[] args) {
+        MeetingRoomController meetingRoomController = new MeetingRoomController();
+        meetingRoomController.runMenu();
+    }
+}
