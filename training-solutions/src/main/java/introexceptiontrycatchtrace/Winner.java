6c805bba975ae0eb7ef469f5ca3b5a2b3e8ec407
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-14 12:56:53 +0100

    winner class updated

--- training-solutions/src/main/java/introexceptiontrycatchtrace/Winner.java
+++ training-solutions/src/main/java/introexceptiontrycatchtrace/Winner.java
@@ -1,13 +1,10 @@
 package introexceptiontrycatchtrace;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.Random;
+import java.util.*;
 
 public class Winner {
-    private List<String> players = Arrays.asList("Anna", "Pál", "Virág", "Frigyes", "Eduárd", null);
-
+    //private List<String> players = Arrays.asList("Anna", "Pál", "Virág", "Frigyes", "Eduárd", null);
+    private List<String> players = new ArrayList<>(Arrays.asList("Sára", "Magdolna", "Frigyes", "Eduárd", null));
     public String getWinner(){
         int winnerIndex = new Random().nextInt(players.size());
         return players.get(winnerIndex).toUpperCase(Locale.ROOT);
