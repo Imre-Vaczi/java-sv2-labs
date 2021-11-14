4e4cabf94840d2d6e778b5d08a098143e19a348d
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-14 14:54:17 +0100

    Calculator class updated

---
+++ training-solutions/src/main/java/introexceptiontrycatchtrace/Calculator.java
@@ -0,0 +1,19 @@
+package introexceptiontrycatchtrace;;
+
+public class Calculator {
+    public int additionOperation(int x, int y){
+        return x + y;
+    }
+
+    public int subtractionOperation(int x, int y){
+        return x - y;
+    }
+
+    public int multiplicationOperation(int x, int y){
+        return x * y;
+    }
+
+    public int divisionOperation(int x, int y){
+        return x / y;
+    }
+}
