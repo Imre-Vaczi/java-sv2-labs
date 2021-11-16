1e98fd5313129e52334409cf1d3e9eed9a8b1fc2
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-16 21:42:19 +0100

    init

--- training-solutions/src/main/java/introexceptionthrow/Validation.java
+++ training-solutions/src/main/java/introexceptionthrow/Validation.java
@@ -7,17 +7,18 @@ public class Validation {
         }
     }
 
+
     public void validateAge(String ageString) {
+        for (char ch : ageString.toCharArray()) {
+            if (!Character.isDigit(ch)) {
+                throw new IllegalArgumentException("A megadott érték nem csak számokat tartalmaz.");
+            }
+        }
         if (ageString.length() ==0){
             throw new IllegalArgumentException("Nem adott meg életkort.");
         }
         if (Integer.parseInt(ageString) < 0 || Integer.parseInt(ageString) > 120) {
             throw new IllegalArgumentException("A megadott életkor nem valid.");
-        }
-        for (char ch : ageString.toCharArray()) {
-            if (!Character.isDigit(ch)) {
-                throw new IllegalArgumentException("A megadott érték nem csak számokat tartalmaz.");
-            }
         }
     }
 }
