ea15d76c8605aa8b7721857c5f45e90f3323404c
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-16 20:56:53 +0100

    init

---
+++ training-solutions/src/main/java/introexceptionthrow/AdultContent.java
@@ -0,0 +1,8 @@
+package introexceptionthrow;
+
+public class AdultContent {
+    public static void main(String[] args) {
+        Adult adultA = new Adult("Frigyes", 30);
+        Adult adultB = new Adult("Frici", 17);
+    }
+}
