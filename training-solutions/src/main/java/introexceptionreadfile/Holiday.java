dbef5cfda6d04c66b4909445bb2c8d71504acb67
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-20 21:28:22 +0100

    init

---
+++ training-solutions/src/main/java/introexceptionreadfile/Holiday.java
@@ -0,0 +1,21 @@
+package introexceptionreadfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Holiday {
+
+    public static void main(String[] args) {
+        try {
+            List<String> content = Files.readAllLines(Paths.get("holiday.txt"));
+            System.out.println(content);
+        }
+        catch (IOException ioException) {
+            System.out.println("Tartalom beolvasása sikertelen");
+            ioException.printStackTrace();
+        }
+    }
+
+}
---
+++ training-solutions/src/main/java/introexceptionreadfile/Shopping.java
@@ -0,0 +1,20 @@
+package introexceptionreadfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Shopping {
+
+    public static void main(String[] args) {
+        try {
+            List<String> grocery = Files.readAllLines(Paths.get("shoppinglist.txt"));
+            System.out.println(grocery);
+        }
+        catch (IOException ioException) {
+            System.out.println("Tartalom beolvasása nem lehetséges.");
+            ioException.printStackTrace();
+        }
+    }
+}
