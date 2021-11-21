63070973b29ece1d8cc5419ec0dd3fde420e92d3
Imre-Vaczi <imre.vaczi@gmail.com>
2021-11-21 13:08:32 +0100

    init

--- training-solutions/src/test/java/introexceptionreadfiletestjunit4/RecipeTest.java
+++ training-solutions/src/test/java/introexceptionreadfiletestjunit4/RecipeTest.java
@@ -1,14 +1,14 @@
 package introexceptionreadfiletestjunit4;
 
-import org.junit.Test;
+/*import org.junit.Test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertThrows;*/
 
 class RecipeTest {
     Recipe recipe = new Recipe();
-
+/*
     @Test
     public void testWithNoIssues(){
         recipe.addIngredients(Paths.get("src/test/resources/recipe_A.txt"));
@@ -20,5 +20,5 @@ class RecipeTest {
         IllegalStateException exception = assertThrows(IllegalStateException.class,
                 () -> recipe.addIngredients(Paths.get("src/test/resources/recipe_B.txt")));
         assertEquals("Beolvasási hiba.", exception.getMessage());
-    }
+    }*/
 }
