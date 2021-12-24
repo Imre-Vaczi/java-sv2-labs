28c51748b0e7bc0ae9ecb9c51ed5704c9a87c7af
Imre-Vaczi <imre.vaczi@gmail.com>
2021-12-24 16:00:19 +0100

    init

---
+++ training-solutions/src/main/java/isahasa/inhabitant/Inhabitant.java
@@ -0,0 +1,19 @@
+package isahasa.inhabitant;
+
+public class Inhabitant extends Person{
+
+    private ParkingPlace parkingPlace;
+
+    public Inhabitant(String name, ParkingPlace parkingPlace) {
+        super(name);
+        this.parkingPlace = parkingPlace;
+    }
+
+    public ParkingPlace getParkingPlace() {
+        return parkingPlace;
+    }
+
+    public void changeParkingPlace(int newParkingPlace) {
+        this.parkingPlace.setNumber(newParkingPlace);
+    }
+}
